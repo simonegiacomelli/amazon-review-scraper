9b3a3cc39ef88dc3957c7333825897a6113cf724
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-08-28 09:58:17 +0200

    development product folder structure

--- products_folder.py
+++ products_folder.py
@@ -1,8 +1,16 @@
+import os
+from pathlib import Path
 
 
 class ProductsFolder:
-    pass
+    def __init__(self, folder='products'):
+        self.folder: Path = Path(folder)
 
     @property
     def groups(self):
-        pass
+        return sorted(os.listdir(self.folder))
+
+    def products_for_group(self, group):
+        group_folder = self.folder / group
+        listdir = [p for p in os.listdir(group_folder) if not p.startswith('.') and (group_folder / p).is_dir()]
+        return sorted(listdir)
--- test_products_folder.py
+++ test_products_folder.py
@@ -1,7 +1,23 @@
 from unittest import TestCase
 
+from products_folder import ProductsFolder
+
 
 class TestProductsFolder(TestCase):
+
     def test_groups(self):
-        # self.fail()
-        pass
+        target = ProductsFolder(folder='test_files/products_structure')
+        self.assertEqual(['group1', 'group2'], target.groups)
+
+    def test_products_for_group(self):
+        target = ProductsFolder(folder='test_files/products_structure')
+
+        expected = ['amazonProductId1--product-title-1', 'amazonProductId2--product-title-2']
+        actual = target.products_for_group('group1')
+        self.assertEqual(expected, actual)
+
+        expected = []
+        actual = target.products_for_group('group2')
+        self.assertEqual(expected, actual)
+
+
