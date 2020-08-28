9749e0aa97f6bf075ab10be28b383fabbe4ae20d
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-08-28 16:10:02 +0200

    add test case to get product_by_name and all_stars

--- products_folder.py
+++ products_folder.py
@@ -9,10 +9,23 @@ class Group:
         self.group_folder = folder / name
 
     @property
-    def products(self):
+    def products(self):  # rename to all_products
         listdir = [p for p in os.listdir(self.group_folder)
                    if not p.startswith('.') and (self.group_folder / p).is_dir()]
         return sorted(listdir)
+
+    def product_by_name(self, product_name):
+        return Product(self, product_name)
+
+
+class Product:
+    def __init__(self, group: Group, product_name):
+        self.group = group
+        self.product_name = product_name
+
+    @property
+    def all_stars(self):
+        return [0] * 5
 
 
 class ProductsFolder:
@@ -20,9 +33,9 @@ class ProductsFolder:
         self.folder: Path = Path(folder)
 
     @property
-    def groups(self):
+    def all_groups(self):
         return [self.group_by_name(n) for n in sorted(os.listdir(self.folder))]
 
-    def group_by_name(self, group_name):
+    def group_by_name(self, group_name) -> Group:
         res = Group(self.folder, group_name)
         return res
--- test_products_folder.py
+++ test_products_folder.py
@@ -9,7 +9,7 @@ class TestProductsFolder(TestCase):
         self.target = ProductsFolder(folder='test_files/products_structure')
 
     def test_groups(self):
-        actual = [g.name for g in self.target.groups]
+        actual = [g.name for g in self.target.all_groups]
         self.assertEqual(['group1', 'group2'], actual)
 
     def test_products_for_group(self):
@@ -20,3 +20,11 @@ class TestProductsFolder(TestCase):
         expected = []
         actual = self.target.group_by_name('group2').products
         self.assertEqual(expected, actual)
+
+    def test_get_star_folders_for_product(self):
+        group1 = self.target.group_by_name('group1')
+        product1 = group1.product_by_name('amazonProductId1--product-title-1')
+        self.assertEqual(5, len(product1.all_stars))
+
+        product2 = group1.product_by_name('amazonProductId2--product-title-2')
+        self.assertEqual(5, len(product2.all_stars))
