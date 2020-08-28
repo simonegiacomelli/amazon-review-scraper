52b9e1d0947858d8b0f35cc479417ae5cf5884b3
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-08-28 10:00:32 +0200

    refactoring

--- test_products_folder.py
+++ test_products_folder.py
@@ -5,19 +5,17 @@ from products_folder import ProductsFolder
 
 class TestProductsFolder(TestCase):
 
+    def setUp(self) -> None:
+        self.target = ProductsFolder(folder='test_files/products_structure')
+
     def test_groups(self):
-        target = ProductsFolder(folder='test_files/products_structure')
-        self.assertEqual(['group1', 'group2'], target.groups)
+        self.assertEqual(['group1', 'group2'], self.target.groups)
 
     def test_products_for_group(self):
-        target = ProductsFolder(folder='test_files/products_structure')
-
         expected = ['amazonProductId1--product-title-1', 'amazonProductId2--product-title-2']
-        actual = target.products_for_group('group1')
+        actual = self.target.products_for_group('group1')
         self.assertEqual(expected, actual)
 
         expected = []
-        actual = target.products_for_group('group2')
+        actual = self.target.products_for_group('group2')
         self.assertEqual(expected, actual)
-
-
